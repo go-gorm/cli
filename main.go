fc62c50431495a20709791fed3e95eb0cf144a89
Miladev95 <miladev95@gmail.com>
2025-12-10 12:04:44 +0330

    feat: add version command to CLI

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"gorm.io/cli/gorm/internal/gen"
 )
 
+// Version can be set at build time via -ldflags "-X main.Version=x.y.z"
+var Version = "v0.2.4"
+
 func main() {
 	rootCmd := &cobra.Command{
 		Use:   "gorm",
@@ -15,9 +18,20 @@ func main() {
 	}
 
 	rootCmd.AddCommand(gen.New())
+	rootCmd.AddCommand(versionCmd())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the version of gorm-cli",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("gorm-cli version %s\n", Version)
+		},
 	}
 }
