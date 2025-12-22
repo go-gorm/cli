60beec6e0975db58e2a00ee01f9573aeffafc61c
Miladev95 <miladev95@gmail.com>
2025-12-22 12:22:24 +0330

    Inline version resolution into `versionCmd`; remove top-level `getVersion`

--- main.go
+++ main.go
@@ -24,22 +24,19 @@ func main() {
 	}
 }
 
-func getVersion() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		if info.Main.Version != "" && info.Main.Version != "(devel)" {
-			return info.Main.Version
-		}
-	}
-
-	return "dev"
-}
-
 func versionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version of gorm-cli",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("gorm-cli version %s\n", getVersion())
+			if info, ok := debug.ReadBuildInfo(); ok {
+				if info.Main.Version != "" && info.Main.Version != "(devel)" {
+					fmt.Printf("gorm-cli version %s\n", info.Main.Version)
+					return
+				}
+			}
+
+			fmt.Printf("gorm-cli version %s\n", "dev")
 		},
 	}
 }
